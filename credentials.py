f8f13ff47bbf9fd05907f516cb6da7692ab84dcc
RPiAwesomeness <aeves.nate@gmail.com>
2015-04-27 13:20:45 -0400

    Minor tweaks

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 # Currently, the only site supported is GMail
 
-USERNAME = 'keyboardmailtesting'	# Replace with your own username
-PASSWORD = 'Ra$dA123'			# Replace with your own password
+USERNAME = 'USERNAME'	# Replace with your own username
+PASSWORD = 'PASSWORD'			# Replace with your own password
