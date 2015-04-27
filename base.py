ad8178993125d71bbd363e27bcd57187209c10f5
RPiAwesomeness <aeves.nate@gmail.com>
2015-04-27 14:59:41 -0400

    Fixed the multiple rescipients bug. Fixed the bug where only the first of multiple recipients actually recieves the message. Still need to figure out how to send the message to multiple recipients all at once, instead of the current method of one by one.

--- base.py
+++ base.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 
 # Keyboard Mail Daemon
-# Version: 		0.0.10
-# Date:			April 26th, 2015
+# Version: 		0.0.11
+# Date:			April 27th, 2015
 # Contributors:	RPiAwesomeness
 """Changelog:
-		Basic HTML formatting has been successfully added as well as the
-		freeze bugs removed.
-		As far as I know, the program should work correctly - the only
-		thing that hasn't been yet confirmed is if multiple recipients
-		are working. However, it is fairly certain that it is, so I'm
-		going ahead with the commit.
-		I'm also removing any deprecated code, including the entire
-		EMessage class (RIP my dear friend, you served me well) and any
-		reference to that class or functions within that class.
-		If there is any need to reference that code in the future, please
-		refer back to the commit history.
+		Fixed bug where only the first of multiple recipients actually
+		recieves the message.
+		Still need to figure out how to send the message to multiple
+		recipients all at once, instead of the current method of one by
+		one.
 """
 
 import smtplib
@@ -26,33 +20,31 @@ from email.mime.text import MIMEText
 def prompt(prompt):
 	return input(prompt).strip()
 
-def send(fromaddr, toaddrs, msg):
+def send(fromaddr, toaddr, msg):		# Sends the message, accepts the from address, to address and message as arguments
 
 	try:
-		with smtplib.SMTP('smtp.gmail.com:587') as server:
-			server.set_debuglevel(1)
-			server.ehlo()
-			server.starttls()
-			server.login(credsUSER, credsPASS)
-			server.send_message(msg, fromaddr, toaddrs)
+		s.send_message(msg, fromaddr, toaddr)	# Send the message.
 	except Exception as e:
-		print(e.args[0])
 		print(e.args)
 
 if __name__ =='__main__':
 
 	msg = MIMEMultipart('alternative')
-	msg['Subject']	= input('Subject: ')
-	msg['From']		= input('From: ')
-	recipients		= input('To: ').split()
-	msg['To']		= ', '.join(recipients)
+	msg['Subject']	= input('Subject: ')	# Subject
 	
+	msg['From']		= input('From: ')		# From
+	fromaddr 		= msg['From']			# From in its own string
+	
+	recipients		= input('To: ').split()	# All of the recipients in list form
+	msg['To'] 		= COMMASPACE.join(recipients)	# One long string of recipients, just for MIME standards, not technically needed.
+	
+	# Enter your email
 	print ('Enter message, end with ^D (Unix/Linux) or ^Z (Windows):')
 	
 	text = ''
-	html = '<html><head></head><body>'
+	html = '<html><head></head><body>'	# Initialize html variable with initial, default HTML tags
 			
-	while True:
+	while True:							# Loops, allowing you to input your message
 		try:
 			line = input()
 		except EOFError:
@@ -62,14 +54,26 @@ if __name__ =='__main__':
 	
 	html += '</body></html>'	# Once loop is complete, close all the tags
 	
-	partPLAINTEXT	= MIMEText(text, 'plain')
-	partHTML		= MIMEText(html, 'html')
+	partPLAINTEXT	= MIMEText(text, 'plain')	# Create MIMEText object for the plaintext version of the body
+	partHTML		= MIMEText(html, 'html')	# Create MIMEText object for the HTML version of the body
 	
-	msg.attach(partPLAINTEXT)
-	msg.attach(partHTML)
-		
-	fromaddr = msg['From']
-	toaddrs  = msg['To']
+	msg.attach(partPLAINTEXT)	# Add the MIMEText object for the plaintext to the message
+	msg.attach(partHTML)		# Add the MIMEText object for the HTML to the message
 	
-	send(fromaddr, toaddrs, msg)
-
+	# Set up connection to server so we don't have  to do it manually each time...
+	
+	try:
+		s = smtplib.SMTP('smtp.gmail.com:587')	# Connect to the smtp.gmail.com site
+		s.set_debuglevel(1)						# Set the debug level so we get verbose feedback from the server
+		s.ehlo()								# Say EHLO to the server
+		s.starttls()							# Attempt to start TLS
+		s.login(credsUSER, credsPASS)			# Log in to the site with the provided username & password (stored in credentials.py)
+	except Exception as e:
+		print(e.args)
+	
+	# Need to find a way to set the msg['To'] field to have multiple recipients because it will be faster.
+	# Currently, each message is sent individually to each recipient.	
+	for recip in recipients:		# Iterate through all of the recipients and ...
+		send(fromaddr, recip, msg)	# Call the send function individually for each.
+	
+	s.quit()						# Done, disconnect from the server
