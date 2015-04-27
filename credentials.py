23e2c777d891fc665991b9973816f5a5b76781dd
RPiAwesomeness <aeves.nate@gmail.com>
2015-04-27 13:43:13 -0400

    Just a test for .gitignore

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 # Currently, the only site supported is GMail
-# Just a test
+# Just a test for .gitignore
 USERNAME = 'USERNAME' 	# Replace with your own username
 PASSWORD = 'PASSWORD'	# Replace with your own password
