673f0c753de13de9292123c68cbd00beccfad723
Nathanael Olander <aeves.nate@gmail.com>
2015-04-26 20:55:42 -0400

    Several appearance changes Moved formatMessage() into the EMessage class to make it look better, re-arraigned the calls to initialize the msg object and to format the body of the email to work with the new system.
    Added "if __name__ == '__main__':" clause to make sure that the main code is only executed if the program is called, not if it is imported as a module, something it probably will be in the future.

--- base.py
+++ base.py
@@ -1,27 +1,35 @@
+#!/usr/bin/python3
+
 # Keyboard Mail Daemon
-# Version: 		0.0.7
+# Version: 		0.0.8
 # Date:			April 26th, 2015
 # Contributors:	RPiAwesomeness
 """Changelog:
-		Introduced credential storage method of credentials.py variables
-		being imported and used.
+		Moved formatMessage() into the EMessage class to make it look
+		better, re-arraigned the calls to initialize the msg object
+		and to format the body of the email to work with the new system.
 		
-		Username/password are now stored in credentials.py and are
-		accessible via credsUSER and credsPASS respectively.
+		Added "if __name__ == '__main__':" clause to make sure that
+		the main code is only executed if the program is called, not if
+		it is imported as a module, something it probably will be in
+		the future.
 """
 
 import smtplib
 from credentials import USERNAME as credsUSER, PASSWORD as credsPASS
+#from email.mime.multipart import MIMEMultipart
+#from email.mime.text import MIMEText
+# Used for next change in which I introduce HTML formatting of the 
+# 	message
 
-class eMessage:
+class EMessage:
 	
-	def __init__(self, fromaddr, toaddrs, subject, content):
+	def __init__(self, fromaddr, toaddrs, subject):
 		self.fromaddr = fromaddr
-		self.to 	  = toaddrs
+		self.toaddrs  = toaddrs
 		self.subject  = subject
-		self.content  = content
 	
-	def send(self):
+	def send(self, content):
 		#username 	= input('GMail username: ')
 		#pswd		= input('Password: ')
 		# Deprecated by credential storage method 
@@ -32,43 +40,48 @@ class eMessage:
 				server.starttls()
 				server.login(credsUSER, credsPASS)
 				server.set_debuglevel(1)
-				server.sendmail(self.fromaddr, self.to, self.content)
+				server.sendmail(self.fromaddr, self.toaddrs, content)
 	
 		except Exception as e:
 			print(e.args[0])
 			if len(e.args) >= 2:
 				print(bytes.decode(e.args[1]))
-			
-def formatMessage(fromaddr, to, subject, content):
-	rtrnMsg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s"
-				% (fromaddr, ", ".join(to), subject, content))
-	return (rtrnMsg)
+	
+	def formatMessage(self, content):
+		rtrnMsg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s"
+				% (self.fromaddr, ", ".join(self.toaddrs), self.subject,
+						content))
+		return rtrnMsg
 		
+				
 def prompt(prompt):
 	return input(prompt).strip()
 
-fromaddr 	= prompt("From: ")
-toaddrs		= prompt("To: ").split()	# If you hard-program, it must be a list
-subject 	= prompt("Subject: ")
+if __name__ =='__main__':
 
-print ("Enter message, end with ^D (Unix/Linux) or ^Z (Windows):")
+	fromaddr 	= prompt("From: ")
+	toaddrs		= prompt("To: ").split()	# If you hard-program, it must be a list
+	subject 	= prompt("Subject: ")
+	
+	print ("Enter message, end with ^D (Unix/Linux) or ^Z (Windows):")
+	
+	# Add the From: and To: headers to the start
+	content = ""	
+			
+	while True:
+		try:
+			line = input()
+		except EOFError:
+			break
+		content = content + '\n' + line
+	#print ("Message length is",len(content))
+	#print ("Message is:",content)
+	
+	msg = EMessage(fromaddr, toaddrs, subject)
+	#print(msg)
+	
+	body = msg.formatMessage(content)
+	#print (body)
+	
+	msg.send(body)
 
-# Add the From: and To: headers to the start
-content = ""	
-		
-while True:
-	try:
-		line = input()
-	except EOFError:
-		break
-	content = content + '\n' + line
-#print ("Message length is",len(content))
-#print ("Message is:",content)
-
-body = formatMessage(fromaddr, toaddrs, subject, content)
-#print (body)
-
-msg = eMessage(fromaddr, toaddrs, subject, body)
-#print(msg)
-
-msg.send()
