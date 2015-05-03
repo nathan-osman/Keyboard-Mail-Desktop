3cb7e8d52e88038bf940d9d7ecfb4de05f4ff35a
Nathanael Olander <aeves.nate@gmail.com>
2015-05-02 21:26:57 -0400

    Added logging capabilities & more error catching Also, added the .gitignore file so people working on the project don't accidentally upload sensitive details.

--- base.py
+++ base.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
 # Keyboard Mail Daemon
-# Version: 		0.0.16
-# Date:			May 1st, 2015
+# Version: 		0.0.17
+# Date:			May 2, 2015
 # Contributors:	RPiAwesomeness
 """Changelog:
-		Uber minor change, removed the From: prompt after asking for subject 
-		since it doesn't actually affect anything other than the MIME
-		header. The *true* from is set by the server when you send the
-		message.
+		Added logging - logs are now stored in the data directory.
+		Added a bunch of error handling.
+		Moved a bunch of code from the if __name__ == '__main__' statement
+		to the main() function which gets called from within the previous
+		if statement for easier management.
 """
 
-import smtplib, imaplib, mimetypes, sys, os, io
+import smtplib, imaplib, mimetypes
+import sys, os, time
+import logging
 
 from email					import encoders
 from email.mime.multipart 	import MIMEMultipart
@@ -38,6 +41,8 @@ def send(fromaddr, toaddr, msg):		# Sends the message, accepts the from address,
 	try:
 		server.send_message(msg, fromaddr, toaddr)	# Send the message.
 	except Exception as e:
+		logging.error('Message failed to send')
+		logging.error('Error:',e.args)
 		print(e.args)
 
 def get_message_data():
@@ -47,6 +52,15 @@ def get_message_data():
 	msg['Subject']	= input('Subject: ')	# Subject	
 	msg['From']		= credsUSER				# From - set by the credential.py file
 	recipients		= input('To: ').split()	# All of the recipients in list form
+	
+	if recipients == '':					# Make sure that there is at least one recipient (need email confirmation)
+		logging.info('No recipients given')
+		print ("At least one recipient is required. Otherwise, you're",
+				"simply sending the message off into pixie land and it",
+				"then dies.")
+		print ('Returning you to the start')
+		main()
+	
 	msg['To'] 		= COMMASPACE.join(recipients)	# One long string of recipients, just for MIME standards, not technically needed.
 	msg['Date']		= str(formatdate(localtime=True))
 	
@@ -82,62 +96,102 @@ def setup_attachment():
 	
 	path = filedialog.askopenfilename()
 	
-	return path
+	print (path)
 	
-def msg_setup(path):
+	if path == '':		# No attachment chosen - acceptable result
+		logging.info('No attachment selected!')
+		attached = False
+	else:				# Attachment chosen
+		logging.info('Attachment ' + path + ' chosen.')
+		attached = True
+			
+	return path, attached
 	
-	ctype, encoding = mimetypes.guess_type(path)
-	if ctype is None or encoding is not None:
-		ctype = 'application/octet-stream'
-	maintype, subtype = ctype.split('/', 1)
-	
-	if maintype == 'text':
-		with open(path) as fp:
-			attachment = MIMEText(fp.read(), _subtype=subtype)
-	elif maintype == 'image':
-		with open(path, 'rb') as fp:
-			attachment = MIMEImage(fp.read(), _subtype=subtype)
-	elif maintype == 'audio':
-		with open(path, 'rb') as fp:
-			attachment = MIMEAudio(fp.read(), _subtype=subtype)
-	else:
-		with open(path, 'rb') as fp:
-			attachment = MIMEBase(maintype, subtype)
-			attachment.set_payload(fp.read())
-	encoders.encode_base64(attachment)
-	attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(path))
-	
-	text, html = get_message_data()		# Get the user's input for the content of the message
-	
-	partPLAINTEXT, partHTML = content_setup(text, html)	# Get the message parts (plaintext and HTML) for MIME
-	
-	return partPLAINTEXT, partHTML, attachment
+def msg_setup(path=''):
 
-if __name__ =='__main__':
+	if path != '':		# Attachment given
+		
+		ctype, encoding = mimetypes.guess_type(path)
+		if ctype is None or encoding is not None:
+			ctype = 'application/octet-stream'
+		maintype, subtype = ctype.split('/', 1)
+		
+		if maintype == 'text':
+			with open(path) as fp:
+				attachment = MIMEText(fp.read(), _subtype=subtype)
+		elif maintype == 'image':
+			with open(path, 'rb') as fp:
+				attachment = MIMEImage(fp.read(), _subtype=subtype)
+		elif maintype == 'audio':
+			with open(path, 'rb') as fp:
+				attachment = MIMEAudio(fp.read(), _subtype=subtype)
+		else:
+			with open(path, 'rb') as fp:
+				attachment = MIMEBase(maintype, subtype)
+				attachment.set_payload(fp.read())
+		encoders.encode_base64(attachment)
+		attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(path))				
+		
+		text, html = get_message_data()		# Get the user's input for the content of the message
+		
+		partPLAINTEXT, partHTML = content_setup(text, html)	# Get the message parts (plaintext and HTML) for MIME
+		
+		logging.info('Message content objects successfully created.')
+	
+		return partPLAINTEXT, partHTML, attachment
+		
+	else:				# No attachment provided
+		
+		logging.info('No attachment')
+		
+		text, html = get_message_data()		# Get the user's input for the content of the message
+		
+		partPLAINTEXT, partHTML = content_setup(text, html)	# Get the message parts (plaintext and HTML) for MIME
+		
+		logging.info('Message content objects successfully created.')
+		
+		return partPLAINTEXT, partHTML
+		
+def main():
 	
 	global attachment, server, msg
 	
 	msg = MIMEMultipart('mixed')	# Initialize overarching message as msg
 	content = MIMEMultipart('alternative')	# This takes the text/html and stores it for the email
 	
-	path = setup_attachment()
+	path, attached = setup_attachment()
 	
-	partPLAINTEXT, partHTML, attachment = msg_setup(path)
+	if attached:					# Attachment is there
+		partPLAINTEXT, partHTML, attachment = msg_setup(path)
+	else:							# No attachment
+		partPLAINTEXT, partHTML = msg_setup()
 	
 	content.attach(partPLAINTEXT)	# Add the MIMEText object for the plaintext to the message
 	content.attach(partHTML)		# Add the MIMEText object for the HTML to the message
 	
+	logging.info('Message length ' +str(len(partPLAINTEXT)))
+	
 	msg.attach(content)			# Attach the content to the message
-	msg.attach(attachment)		# Attach the attachment to the message	
+	
+	if attached:				# Attachment is there
+		msg.attach(attachment)		# Attach the attachment to the message
 	
 	# Set up connection to server so we don't have repeat it each time
 	try:
 		server = smtplib.SMTP('smtp.gmail.com:587')	# Connect to smtp.gmail.com, port 587
+		
+		logging.info('Connected to Gmail SMTP server successfully')
+		
 		server.set_debuglevel(1)					# Set the debug level so we get verbose feedback from the server
 		server.ehlo()								# Say EHLO to the server
 		server.starttls()							# Attempt to start TLS
 		server.login(credsUSER, credsPASS)			# Log in to the site with the provided username & password (stored in credentials.py)
+		
+		logging.info('User logged in successfully')
+		
 	except Exception as e:
+		logging.error('Error during SMTP')
+		logging.error('Error:',e.args)
 		print(e.args)
 		quit()
 	
@@ -148,4 +202,28 @@ if __name__ =='__main__':
 	for recip in recipients:		# Iterate through all of the recipients and ...
 		send(fromaddr, recip, msg)	# Call the send function individually for each
 	
+	logging.info('Message sent successfully.')
+	
 	server.quit()						# Done, disconnect from the server
+	
+	logging.info('Server disconnected')
+	
+	quit()
+
+if __name__ =='__main__':
+	
+	global attached
+	
+	attached = False
+	
+	logPath = ('data/'+time.strftime("%d-%m-%Y")+'_'+time.strftime("%I-%M-%S %p"))
+	
+	logging.basicConfig(
+		format='%(asctime)s :: %(levelname)s :: %(message)s', 
+		datefmt='%m/%d/%Y %I:%M:%S %p',
+		filename=logPath, 
+		level=logging.DEBUG)
+			
+	logging.info('Program Start')
+	
+	main()
